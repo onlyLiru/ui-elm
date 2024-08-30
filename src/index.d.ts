e3dbb2684a35e8166c69704a7d383d1a9eaa2e83
tony <wb-lr903313@alibaba-inc.com>
2024-08-30 10:00:45 +0800

    formatNumber

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './components/Button/Button';
 export { default as StyledButton } from './components/StyledButton/Button';
+export * as utils from './utils';
---
+++ src/utils/formatNumber.ts
@@ -0,0 +1,42 @@
+/**
+
+格式化数字
+@param num 要格式化的数字
+@param options 格式化选项
+@param options.format 格式化的类型，可选值为'money'（货币），'int'（整数），'percent'（百分比），'origin'（原始）
+@param options.percentagePrecision 百分比类型的精度，可选
+@param options.precision 货币类型的精度，可选
+@returns 格式化后的数字
+*/
+
+export function formatNumber(num: number, options: { format: 'money' | 'int' | 'percent' | 'origin', percentagePrecision?: number, precision?: number }): any {
+  let formattedNumber: string;
+  if (isNaN(num)) { return num }
+
+  const { precision } = options;
+
+  switch (options.format) {
+    case 'money':
+      if (typeof precision === 'number') {
+        formattedNumber = num.toFixed(precision).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+      } else {
+        formattedNumber = num.toLocaleString();
+      }
+      break;
+    case 'int':
+      formattedNumber = Math.round(num).toString();
+      break;
+    case 'percent':
+      formattedNumber = (num * 100).toFixed(options.percentagePrecision) + '%';
+      break;
+    case 'origin':
+      formattedNumber = num.toString();
+      break;
+    default:
+      formattedNumber = num.toString();
+      break;
+  }
+
+  return formattedNumber;
+}
+
