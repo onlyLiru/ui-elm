cf724f06352a66abb240f0bf0c774f70c81f6dba
tony <wb-lr903313@alibaba-inc.com>
2024-09-04 15:02:38 +0800

    storybook 注释

--- src/components/StyledButton/Button.tsx
+++ src/components/StyledButton/Button.tsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
 
-export type ButtonProps = {
+export interface ButtonProps {
+  /** 背景色 */
   children?: React.ReactNode | string;
+  /**
+   * children也可以使用label代替
+   */
   label?: string;
   /** 背景色 */
   backgroundColor?: string;
@@ -13,33 +17,31 @@ export type ButtonProps = {
    */
   primary?: boolean;
   /**
-   * 按钮的尺寸
+   * How large should the button be?
    */
   size?: 'small' | 'medium' | 'large';
   /** 自定义样式style */
   style?: any
 };
 
-const sizeMap = {
-  small: '0.2rem 0.4rem',
-  medium: '0.4rem 0.8rem',
-  large: '0.6rem 1.2rem',
-}
+/**
+ * Primary UI component for user interaction
+ */
 
-const Button = tw.button<ButtonProps>`
+const ButtonComponent = tw.button<ButtonProps>`
   ${p => p.primary ? 'text-white' : 'text-[rgba(0,0,0,.65)]'}
-  px-2
+  px-4
   py-1
-  ${p => p.size === 'small' && 'px-2'}}
+  ${p => p.size === 'small' && ('px-2 py-0')}}
   ${p => p.size === 'medium' && 'px-4'}}
   ${p => p.size === 'large' && 'px-6'}}
   rounded-sm
   hover:opacity-80
 `;
-const AddonButton = styled(Button) <ButtonProps>`
+const AddonButton = styled(ButtonComponent) <ButtonProps>`
   background:${props => props.backgroundColor ? props.backgroundColor : (props.primary ? '#f90' : '#d9d9d9')};
 `
 
-export default (props: ButtonProps) => (
+export const Button = (props: ButtonProps) => (
   <AddonButton {...props}>{props.children || props.label}</AddonButton>
 );
