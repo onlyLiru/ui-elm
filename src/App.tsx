0a5b38ea9eedc56942beab59810da221d211b10d
tony <wb-lr903313@alibaba-inc.com>
2024-09-04 11:34:00 +0800

    style button

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
 import { useState } from 'react';
 import Button from './components/StyledButton/Button';
-import { formatNumber } from './utils';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const formattedMoney = formatNumber(1234567.8, { format: 'money', precision: 2 });
-  console.log(formattedMoney); // Output: "1,234,567.89"
-
-  const formattedMoney1 = formatNumber(1234567.8, { format: 'money' });
-  console.log(formattedMoney1); // Output: "1,234,567.89"
-
-  const formattedInt = formatNumber(1234567.89, { format: 'int' });
-  console.log(formattedInt); // Output: "1234568"
-
-  const formattedPercent = formatNumber(0.123, { format: 'percent', precision: 2 });
-  console.log(formattedPercent); // Output: "12.30%"
-
-  const formattedPercent1 = formatNumber(0.123, { format: 'percent' });
-  console.log(formattedPercent1); // Output: "12.30%"
-
-  console.log(formatNumber(0.1234, { format: 'money' }));
-
 
   return (
     <>
-      <Button>Button</Button>
+      <Button backgroundColor='#ff0' style={{ padding: '30px', width: '200px' }}>Button</Button>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- src/components/StyledButton/Button.tsx
+++ src/components/StyledButton/Button.tsx
@@ -16,6 +16,8 @@ export type ButtonProps = {
    * 按钮的尺寸
    */
   size?: 'small' | 'medium' | 'large';
+  /** 自定义样式style */
+  style?: any
 };
 
 const sizeMap = {
@@ -25,13 +27,19 @@ const sizeMap = {
 }
 
 const Button = tw.button<ButtonProps>`
-  ${p => p.backgroundColor ? `bg-[${p.backgroundColor}]` : (p.primary ? 'bg-[#f90]' : 'bg-[#d9d9d9]')}
   ${p => p.primary ? 'text-white' : 'text-[rgba(0,0,0,.65)]'}
-  px-4
+  px-2
   py-1
+  ${p => p.size === 'small' && 'px-2'}}
+  ${p => p.size === 'medium' && 'px-4'}}
+  ${p => p.size === 'large' && 'px-6'}}
   rounded-sm
+  hover:opacity-80
 `;
+const AddonButton = styled(Button) <ButtonProps>`
+  background:${props => props.backgroundColor ? props.backgroundColor : (props.primary ? '#f90' : '#d9d9d9')};
+`
 
 export default (props: ButtonProps) => (
-  <Button {...props}>{props.children || props.label}</Button>
+  <AddonButton {...props}>{props.children || props.label}</AddonButton>
 );
